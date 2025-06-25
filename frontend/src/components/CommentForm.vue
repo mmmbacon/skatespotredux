97e5545c705db06ca429e1a79a7ac9a7ec4f1d18
Brandon Macdonald <bmacdonald1986@gmail.com>
2025-06-24 23:34:36 -0600

    Make Post Comment buttons use primary color to match Login button
    - Update all Post Comment buttons in CommentForm to use color='primary'
    - Ensures consistent blue styling across the application
    - Matches the visual style of the Login with Google button

--- frontend/src/components/CommentForm.vue
+++ frontend/src/components/CommentForm.vue
@@ -44,14 +44,14 @@ const handleLogin = () => {
           class="w-full px-3 py-2 border rounded-lg mb-2"
           required
         ></textarea>
-        <BaseButton type="submit" variant="default" size="sm">
+        <BaseButton type="submit" color="primary" size="sm">
           Post Comment
         </BaseButton>
       </template>
       <template v-else>
         <BaseButton
           type="button"
-          variant="default"
+          color="primary"
           size="sm"
           @click="handleShowField"
         >
@@ -62,7 +62,7 @@ const handleLogin = () => {
     <template v-else>
       <BaseButton
         type="button"
-        variant="default"
+        color="primary"
         size="sm"
         @click="handleShowField"
       >
